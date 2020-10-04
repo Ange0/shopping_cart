a47632c5f09338b09bd11f2a46578e6577353472
Yao kouassi Ange <kouassiangey@gmail.com>
2020-10-04 16:02:43 +0200

    CREATION_CONTOLLER_PRODUCTCONTROLLER_ET_RECUPERATION_DE_TOUS_LES_PRODUITS

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,19 +5,25 @@
 @endsection
 @section('content')
 
-    <div class="col-sm-6 col-md-3">
-        <div class="thumbnail">
-            <img src="images/1.jpg"alt="Generic placeholder thumbnail">
-        </div>
-        <div class="caption">
-            <h3>Product title</h3>
-            <p class="text-justify text-muted" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, sint? Est assumenda quae eveniet nam quaerat molestiae, ad beatae, excepturi iusto soluta, accusamus eius aspernatur eos dolorum optio iste totam?</p>
-            <p class="price">Price :40€</p>
-            <div>
-                <a href="#" class="btn btn-success" role="button"> Add card</a>
-               {{--  <a href="#" class="btn btn-default" role="button">Button</a> --}}
+   @foreach ($products as $product)
+    <div class="row">
+       
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+            <img src="{{$product->imagePath }}"alt="Generic placeholder thumbnail">
+            </div>
+            <div class="caption">
+            <h3>{{ $product->title }}</h3>
+                <p class="text-justify text-muted" >{{ $product->description }}</p>
+            <p class="price">Price :{{ $product->price }}€</p>
+                <div>
+                    <a href="#" class="btn btn-success" role="button"> Add card</a>
+                {{--  <a href="#" class="btn btn-default" role="button">Button</a> --}}
+                </div>
             </div>
         </div>
+       
     </div>
+   @endforeach
 </div>
 @endsection
